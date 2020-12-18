ebefa827f3a7e549474360c19a1b0651b2554f7d
suma2001 <44520637+suma2001@users.noreply.github.com>
2020-12-18 21:26:32 +0000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -9,53 +9,78 @@ import { withAuthenticator } from 'aws-amplify-react';
 import awsconfig from './aws-exports';
 Amplify.configure(awsconfig);
 
-const App = () => {
-  const [imageUrl, setImageUrl] = useState(null);
-  const [loading, setLoading] = useState(false);
-
-  const downloadUrl = async () => {
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+      this.state = {
+        value: "",
+        imageUrl: null,
+        loading: false
+      }
+      this.handleChange = this.handleChange.bind(this)
+  }
+  
+  downloadUrl = async () => {
     // Creates download url that expires in 5 minutes/ 300 seconds
-    const downloadUrl = await Storage.get('picture.jpg', { expires: 300 });
+    const downloadUrl = await Storage.get('image.jpg', { expires: 300 });
     window.location.href = downloadUrl
   }
 
-  const handleChange = async (e) => {
+  handleNewChange = (e) => {
+      this.setState({value: e.target.value})
+      console.log(this.state.value)
+  }
+
+  handleSubmit(event) {
+    alert("A name submitted: " + this.state.value)
+  }
+
+  handleChange = async (e) => {
     const file = e.target.files[0];
     try {
-      setLoading(true);
+      this.setState({loading: true})
       // Upload the file to s3 with private access level. 
-      await Storage.put('picture.jpg', file, {
+      await Storage.put('image.jpg', file, {
         level: 'private',
         contentType: 'image/jpg',
         metadata: {
-          "fullname": "Trump"
+          "fullname": this.state.value
         }
       });
+      console.log("Fullname is : ", this.state.value);
       // Retrieve the uploaded file to display
-      const url = await Storage.get('picture.jpg', { level: 'private' })
-      setImageUrl(url);
-      setLoading(false);
+      const url = await Storage.get('image.jpg', { level: 'private' })
+      this.setState({imageUrl: url})
+      this.setState({loading: false})
+      // logSingleItem();
     } catch (err) {
       console.log(err);
     }
   }
-
-  return (
-    <div className="App">
-      <h1> Upload an Image </h1>
-      {loading ? <h3>Uploading...</h3> : <input
-        type="file" accept='image/jpg'
-        onChange={(evt) => handleChange(evt)}
-      />}
-      <div>
-        {imageUrl ? <img style={{ width: "30rem" }} src={imageUrl} /> : <span />}
+  render() {
+    return (
+      <div className="App">
+        <form onSubmit={this.handleSubmit}>
+          <label>Enter the name: </label>
+          <input type="text" value={this.state.value} onChange={this.handleNewChange} />
+        </form>
+        <h1> Upload an Image </h1>
+        {this.state.loading ? <h3>Uploading...</h3> : <input
+          type="file" accept='image/jpg'
+          onChange={(evt) => this.handleChange(evt)}
+        />}
+        <div>
+          {this.state.imageUrl ? <img style={{ width: "30rem" }} src={this.state.imageUrl} /> : <span />}
+        </div>
+        <div>
+          <h2>Download URL?</h2>
+          <button onClick={() => this.state.downloadUrl()}>Click Here!</button>
+        </div>
+        
       </div>
-      <div>
-        <h2>Download URL?</h2>
-        <button onClick={() => downloadUrl()}>Click Here!</button>
-      </div>
-    </div>
-  );
+    );
+  }
+  
 }
 
 // withAuthenticator wraps your App with a Login component
